9849ed0e7bebb45e08b4a602c2ebce1a96a95385
caijy <c18268151702@163.com>
2023-07-21 17:27:04 +0800

    SpringBoot项目

---
+++ spring-series/spring-series-app/src/main/java/com/xiaobaicai/spring/app/DemoApplication.java
@@ -0,0 +1,19 @@
+package com.xiaobaicai.spring.app;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author caijy
+ * @description TODO
+ * @date 2023/7/21 星期五 5:23 下午
+ */
+
+@Slf4j
+@SpringBootApplication
+public class DemoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+}
