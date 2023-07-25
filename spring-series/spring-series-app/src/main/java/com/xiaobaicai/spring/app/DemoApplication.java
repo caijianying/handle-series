7658c5e5c17562e4e6be394604f8be3653a4b9af
caijy <c18268151702@163.com>
2023-07-25 17:14:13 +0800

    SpringBean的生命周期

--- spring-series/spring-series-app/src/main/java/com/xiaobaicai/spring/app/DemoApplication.java
+++ spring-series/spring-series-app/src/main/java/com/xiaobaicai/spring/app/DemoApplication.java
@@ -1,19 +1,38 @@
 package com.xiaobaicai.spring.app;
 
+import com.xiaobaicai.spring.app.bean.TestBeanCreate;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 /**
- * @author caijy
- * @description TODO
- * @date 2023/7/21 星期五 5:23 下午
- */
-
+ * @Author caijy
+ * @Description Spring演示项目
+ * @Date 2023/7/25 4:05 下午
+ **/
 @Slf4j
 @SpringBootApplication
 public class DemoApplication {
+
+    /**
+     * 演示配置类创建
+     **/
+    public DemoApplication() {
+        log.info("DemoApplication created...");
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
+
+    /**
+     * 演示@Bean创建
+     **/
+    @Bean
+    public TestBeanCreate testBeanCreate() {
+        return new TestBeanCreate();
+    }
 }
+
+
---
+++ spring-series/spring-series-app/src/main/java/com/xiaobaicai/spring/app/bean/TestBeanCreate.java
@@ -0,0 +1,17 @@
+package com.xiaobaicai.spring.app.bean;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @author caijy
+ * @description 演示Bean创建
+ * @date 2023/7/22 星期六 2:25 下午
+ */
+@Slf4j
+public class TestBeanCreate {
+
+    public TestBeanCreate() {
+        log.info("TestService created...");
+    }
+
+}
